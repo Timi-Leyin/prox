a4865eea9fb2b56236f9b6e870a34febfd1795cb
Original Timi <timileyinoyelekan11@gmail.com>
2024-09-05 11:36:53 +0100

    feat: setup constants

---
+++ src/constants.rs
@@ -0,0 +1,9 @@
+// METADATA
+pub const AUTHOR:&str="Timileyin Oyelekan(Original Timi)";
+pub const VERSION:&str="0.0.1";
+pub const ABOUT:&str="Prox is a lightweight caching proxy server built with Rust that forwards requests to an origin server, caches the responses, and serves cached responses when available. It enhances performance by reducing redundant requests and improving response times.";
+pub const INFO:&str="A Caching Proxy Server";
+
+
+pub const MAX_CACHE_SIZE:usize = 1024 * 1024 * 1024;
+pub const DEFAULT_PORT:u16 = 8080;
